a32bf9974b86a5ab43319b0dd52648d035dc9324
sciboy12 <48741660+sciboy12@users.noreply.github.com>
2021-11-15 01:50:08 -0500

    Change engine to kaldi-active-grammar
    Also Cleaned up code/requirements.

--- main.py
+++ main.py
@@ -1,51 +1,55 @@
-import time
-import pythoncom
-from dragonfly import Grammar, MappingRule, Key, Text, Function
+import dragonfly
 import keyboard
 
-enable_ptt = True
+enable_ptt = False
 
 def SendMessage(msg):
     if enable_ptt:
-        if keyboard.is_pressed('l'):
-            print('test')
+        if keyboard.is_pressed('o'):
             keyboard.write(msg)
     else:
         keyboard.write(msg)
         
 # List of messages/commands
-class MappingRule(MappingRule):
+class MappingRule(dragonfly.MappingRule):
     mapping = {
-            "i got it":                 Function(SendMessage, msg="11"),
-            "incoming":                 Function(SendMessage, msg="12"),
-            "go for it":                Function(SendMessage, msg="13"),
-            "defending":                Function(SendMessage, msg="14"),
-            "nice shot":                Function(SendMessage, msg="21"),
-            "what a save":              Function(SendMessage, msg="22"),
-            "thanks":                   Function(SendMessage, msg="23"),
-            "great pass":               Function(SendMessage, msg="24"),
-            "calculated":               Function(SendMessage, msg="31"),
-            "whew|phew":                Function(SendMessage, msg="32"),
-            "wow":                      Function(SendMessage, msg="33"),
-            "close one":                Function(SendMessage, msg="34"),
-            "dang it|darn it":          Function(SendMessage, msg="41"),
-            "no problem":               Function(SendMessage, msg="42"),
-            "whoops|oops":              Function(SendMessage, msg="43"),
-            "sorry":                    Function(SendMessage, msg="44"),
-            "good game|G. G.":          Function(SendMessage, msg="11"),
-            "well played":              Function(SendMessage, msg="12"),
-            "that was fun":             Function(SendMessage, msg="13"),
-            "what a game":              Function(SendMessage, msg="14")
+            "i got it":         dragonfly.Function(SendMessage, msg="11"),
+            "incoming":         dragonfly.Function(SendMessage, msg="12"),
+            "go for it":        dragonfly.Function(SendMessage, msg="13"),
+            "defending":        dragonfly.Function(SendMessage, msg="14"),
+            "nice shot":        dragonfly.Function(SendMessage, msg="21"),
+            "what a play":      dragonfly.Function(SendMessage, msg="22"),
+            "thanks":           dragonfly.Function(SendMessage, msg="23"),
+            "great pass":       dragonfly.Function(SendMessage, msg="24"),
+            "calculated":       dragonfly.Function(SendMessage, msg="31"),
+            "whew|phew":        dragonfly.Function(SendMessage, msg="32"),
+            "wow":              dragonfly.Function(SendMessage, msg="33"),
+            "close one":        dragonfly.Function(SendMessage, msg="34"),
+            "dang it|darn it":  dragonfly.Function(SendMessage, msg="41"),
+            "no problem":       dragonfly.Function(SendMessage, msg="42"),
+            "whoops|oops":      dragonfly.Function(SendMessage, msg="43"),
+            "sorry":            dragonfly.Function(SendMessage, msg="44"),
+            "good game|g g":    dragonfly.Function(SendMessage, msg="11"),
+            "well played":      dragonfly.Function(SendMessage, msg="12"),
+            "that was fun":     dragonfly.Function(SendMessage, msg="13"),
+            "what a game":      dragonfly.Function(SendMessage, msg="14")
             }
 
+# Init Kaldi engine
+engine = dragonfly.get_engine("kaldi",
+    model_dir='kaldi_model',
+    # input_device_index=None,  # set to an int to choose a non-default microphone
+)
+
+# Call connect() now that the engine configuration is set.
+engine.connect()
+
 # Voice command rule combining spoken form and recognition processing.
-grammar = Grammar('grammar') # Create a grammar to contain the command rules 
+grammar = dragonfly.Grammar('grammar') # Create a grammar to contain the command rules 
 
 # Add the command rule to the grammar.
 grammar.add_rule(MappingRule())
 grammar.load()
+# Enter recognition loop
 print('Ready')
-
-while True:
-    pythoncom.PumpWaitingMessages()
-    time.sleep(0.1)
+engine.do_recognition()
