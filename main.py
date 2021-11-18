ffe48da4caab62785bf55068ff5e7e49dbec4ad8
sciboy12 <48741660+sciboy12@users.noreply.github.com>
2021-11-18 14:29:04 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,48 +1,66 @@
 import dragonfly
 import pyautogui
 
-enable_ptt = False
+EnablePTT = False
+EnableDictation = True
+AutoSend = False
 
-def SendMessage(msg):
-    if enable_ptt and keyboard.is_pressed('o'):
-        pyautogui.write(msg, interval = 0.1)
+def Write(msg, enter = False):
+    pyautogui.write(msg, interval = 0.1)
+
+def WriteMessage(msg, *enter):
+    if EnablePTT and keyboard.is_pressed('o'):
+        Write(msg, *enter)
     else:
-        pyautogui.write(msg, interval = 0.1)
+        Write(msg, *enter)
+
+def WriteText(msg):
+    if EnableDictation:
+        pyautogui.press('enter')
+        WriteMessage(msg, *enter)
+        
+    if AutoSend:
+        pyautogui.press('enter')
 
 # List of messages/commands
 class MappingRule(dragonfly.MappingRule):
     mapping = {
             # Imformational
-            "i got it":             dragonfly.Function(SendMessage, msg="11"),
-            "incoming":             dragonfly.Function(SendMessage, msg="12"),
-            "go for it":            dragonfly.Function(SendMessage, msg="13"),
-            "defending":            dragonfly.Function(SendMessage, msg="14"),
+            "i got it":             dragonfly.Function(WriteMessage, msg="11"),
+            "incoming":             dragonfly.Function(WriteMessage, msg="12"),
+            "go for it":            dragonfly.Function(WriteMessage, msg="13"),
+            "defending":            dragonfly.Function(WriteMessage, msg="14"),
             
             # Compliments
-            "nice shot":            dragonfly.Function(SendMessage, msg="21"),
-            "what a save":          dragonfly.Function(SendMessage, msg="22"),
-            "thanks":               dragonfly.Function(SendMessage, msg="23"),
-            "great pass":           dragonfly.Function(SendMessage, msg="24"),
+            "nice shot":            dragonfly.Function(WriteMessage, msg="21"),
+            "what a save":          dragonfly.Function(WriteMessage, msg="22"),
+            "thanks":               dragonfly.Function(WriteMessage, msg="23"),
+            "great pass":           dragonfly.Function(WriteMessage, msg="24"),
 
             # Reactions
-            "calculated":           dragonfly.Function(SendMessage, msg="31"),
-            "what a play":          dragonfly.Function(SendMessage, msg="32"),
-            "wow":                  dragonfly.Function(SendMessage, msg="33"),
-            "close one":            dragonfly.Function(SendMessage, msg="34"),
+            "calculated":           dragonfly.Function(WriteMessage, msg="31"),
+            "what a play":          dragonfly.Function(WriteMessage, msg="32"),
+            "wow":                  dragonfly.Function(WriteMessage, msg="33"),
+            "close one":            dragonfly.Function(WriteMessage, msg="34"),
 
             # Apologies
-            "dang|darn|crap|shit":  dragonfly.Function(SendMessage, msg="41"),
-            "no problem":           dragonfly.Function(SendMessage, msg="42"),
-            "whoops":               dragonfly.Function(SendMessage, msg="43"),
-            "sorry":                dragonfly.Function(SendMessage, msg="44"),
+            "dang|darn|crap|shit":  dragonfly.Function(WriteMessage, msg="41"),
+            "no problem":           dragonfly.Function(WriteMessage, msg="42"),
+            "whoops":               dragonfly.Function(WriteMessage, msg="43"),
+            "sorry":                dragonfly.Function(WriteMessage, msg="44"),
             
             # Post game
-            "good game|g g":        dragonfly.Function(SendMessage, msg="11"),
-            "well played":          dragonfly.Function(SendMessage, msg="12"),
-            "that was fun":         dragonfly.Function(SendMessage, msg="13"),
-            "what a game":          dragonfly.Function(SendMessage, msg="14")
+            "good game|g g":        dragonfly.Function(WriteMessage, msg="11"),
+            "well played":          dragonfly.Function(WriteMessage, msg="12"),
+            "that was fun":         dragonfly.Function(WriteMessage, msg="13"),
+            "what a game":          dragonfly.Function(WriteMessage, msg="14"),
+
+            # Text chat
+            "send <msg>":       dragonfly.Function(lambda text: Sendtext(msg))
             }
 
+    extras = [ dragonfly.Dictation("msg") ]
+    
 # Init Kaldi engine
 engine = dragonfly.get_engine("kaldi",
     model_dir='kaldi_model',
